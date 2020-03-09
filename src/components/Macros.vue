9d20fea05d5f82bb0aff785c0da566a23d1e5ebb
Cole Gentry <Peapod2007@gmail.com>
2020-03-09 19:13:52 -0400

    Adds macros component
    Signed-off-by: Cole Gentry <Peapod2007@gmail.com>

---
+++ src/components/Macros.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="macros">
+    <span aria-label="" title="" class="red">{{ red }}</span>
+    <span aria-label="" title="" class="blue">{{ blue }}</span>
+    <span aria-label="" title="" class="gold">{{ gold }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      red: "20g",
+      blue: "16g",
+      gold: "6g"
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.macros {
+  margin-top: 12px;
+  margin-bottom: 12px;
+
+  span {
+    padding-right: 3px;
+  }
+
+  .red:before,
+  .blue:before,
+  .gold:before {
+    display: inline-block;
+    content: "•";
+    padding-right: 5px;
+    font-size: 16px;
+    height: 6px;
+    width: 6px;
+  }
+  .red:before {
+    color: #f94642;
+  }
+  .blue:before {
+    color: #3177bb;
+  }
+  .gold:before {
+    color: #fda120;
+  }
+}
+</style>
